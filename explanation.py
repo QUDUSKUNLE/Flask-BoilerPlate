251ed2cbe2dcb79a398740ca00a832dc145a9a4b
abdulquddus <qudus.yekeen@andela.com>
2018-01-08 13:02:19 +0100

    [Intoduction to Flask framework]

---
+++ explanation.py
@@ -0,0 +1,22 @@
+"""
+Installation of Flask and its prerequisites
+Recommended file and directory structure for a Flask project
+Configuration and initialization of a Flask app
+Creating views and templates
+
+# Steps to set up flask
+
+We'll need the following installed for this tutorial:
+
+    1. Python (this tutorial uses Python 2)
+    2. virtualenv and virtualenvwrapper
+    3. Flask
+
+We need to use `virtual environments to keep the dependencies used by different
+Python projects separate`, and to keep our global site-packages directory clean.
+
+We'll go one step further and install virtualenvwrapper,
+a set of extensions that make using virtualenv a whole lot easier by providing
+simpler commands.
+
+"""
